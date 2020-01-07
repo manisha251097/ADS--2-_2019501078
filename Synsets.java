c74cc344f3ac892ec1e5af680b1b82fdb0655e5f
manisha251097 <55689527+manisha251097@users.noreply.github.com>
2020-01-07 10:10:14 +0530

    Add files via upload

---
+++ Synsets.java
@@ -0,0 +1,22 @@
+import java.io.*; 
+public class Synsets 
+{ 
+  public static void main(String[] args) throws Exception  { 
+    String fileName = "C:\\Users\\Manisha\\Desktop\\ADS_2\\Module_1\\wordnet\\synsets.txt";
+File file  =  new File(fileName);
+FileReader  obj = new  FileReader(file);
+BufferedReader obj1 = new BufferedReader(obj);
+String line;
+int i=0;
+while((line = obj1.readLine()) != null ){
+  if(i==5) break;
+  String[] arr= line.split(",");
+  for(int j=0;j<arr.length;j++){
+
+    System.out.println(arr[j]);
+  }
+  i++;
+}
+  }
+
+} 
