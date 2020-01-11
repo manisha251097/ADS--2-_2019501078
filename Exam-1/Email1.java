6617fcf63d40ce70e6401452f39c247e1352f186
manisha251097 <55689527+manisha251097@users.noreply.github.com>
2020-01-11 12:37:45 +0530

    Add files via upload

--- Exam-1/Email1.java
+++ Exam-1/Email1.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-public class Email1 {    
-    static ArrayList<String> key= new ArrayList<String>();
-    static ArrayList<String[]> val= new ArrayList<String[]>();
+public class Email1 { 
+    Digraph d;
+    d=new diagraph(logs.size()); 
+    HashMap<String , integer> email2= new HashMap<>();
+    ArrayList<Integer ,Integer> val= new ArrayList[]>();
 
-    public static void  email1(String em) throws Exception{
-
-        
+    public static void email1(String em) throws Exception{
         String fileName = "C:\\Users\\Manisha\\Desktop\\ADS_2\\Exam-1\\emails.txt";
         File file = new File(fileName);
         FileReader fr = new FileReader(file);
@@ -15,36 +15,38 @@ public class Email1 {
         int vcount=0;
         String line;
         while((line  = br.readLine())  != null) {
-            String[] arr = line.split(";",2);
-            if(arr.length>1) {
-                key.add(arr[0]);
-                val.add(arr[1].split(";"));
-            }
-            else {
-                key.add(arr[0]);
-                val.add(null);
-            }
-            vcount++;
+            String[] arr = line.split(";");
+            int e= Integer.parseInt(arr[0]);
+             email2.put(e,arr[1]);
         }
-        System.out.println("No of vertices:"+vcount);
+    
     }
+    ArrayList<Integer[]> logs = new ArrayList<>();
 
+    void emailLogs() throws FileNotFoundException {
+        File f = new File(yourfilename);
+        Scanner s = new Scanner(f);
+        while (s.hasNextLine()) {
+            String[] s1 = s.nextLine().split(",");
+            String[] s2 = s1[0].split(" ");
+            String[] s3 = s1[1].split(" ");
+            Integer[] a = new Integer[2];
+            a[0] = Integer.parseInt(s2[1]);
+            a[1] = Integer.parseInt(s3[s3.length - 1]);
+            logs.add(a);
+        }
+        System.out.println(Arrays.toString(logs.get(0)));
 
-    public static void main(String [] args)throws Exception 
-    {
-        email1("emails");
-        Graph g = new Graph(key.size());
-        int count  =0;
-        for(int i=0;i<key.size();i++) {
-            if(val.get(i)!=null) {
-                for(int j=0; j<val.get(i).length;j++) {
-                    int  k=Integer.parseInt(key.get(i));
-                    int  v=Integer.parseInt(val.get(i)[j]);
-                     g.addEdge(k,v);
-                    count++;
-                }
+    }
+    public void addedge(){
+        email1();
+        emailLogs();
+        d=new diagraph(logs.size());
+        for(int i=0;i<logs.size();i++){
+            for(int j:logs.get(i)){
+                d.addedge(i,j);
             }
         }
-        System.out.println("No of edges:"+count);
+        
     }
-}
+        
